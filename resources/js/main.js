2789c7e266c62576f6be170aaf46df522bdec873
-kmraselbd- <mrkhan20105@gmail.com>
2021-06-07 20:35:03 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,30 @@
+//MixitUp
+var mixer = mixitup('.grid-works-container');
+
+//scrollify
+$(function() {
+    $.scrollify({
+      section : ".scrollify",
+      setHeights: false,
+      updateHash: false,  
+    });
+});
+
+//Mumburger menu
+function openMenu(){
+  document.getElementById('navbar').style.height = "100%"
+}
+
+function closeMenu(){
+  document.getElementById('navbar').style.height = "0%"
+}
+
+//Smooth Scroll
+$(function(){
+  $('.menu-item a, .scroll-down a').on('click', function(){
+    $('html,body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+    return false;
+  });
+});
